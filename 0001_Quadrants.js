9c269b3da9d08f98ae790ea228ec8ab409aee677
Daniel Forero <99362093+ForeroDaniel@users.noreply.github.com>
2023-05-01 12:20:08 -0500

    Quadrants done

--- 0001_Quadrants.js
+++ 0001_Quadrants.js
@@ -1,3 +1,13 @@
 function quadrant(x, y) {
+  let cuadrantPoint;
+    if (x>0 && y>0)
+    cuadrantPoint = 1;
+    else if (x<0 && y>0)
+    cuadrantPoint = 2;
+    else if (x<0 && y<0)
+    cuadrantPoint = 3;
+    else if (x>0 && y<0)
+    cuadrantPoint = 4;
   
+  return cuadrantPoint;
 }
