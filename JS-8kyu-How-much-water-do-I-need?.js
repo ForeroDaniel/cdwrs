66353bd8cf5ba4d568e59f828082617bd06d6fb9
Daniel Forero <99362093+ForeroDaniel@users.noreply.github.com>
2023-07-28 12:02:20 -0500

    Create JS-8kyu-How-much-water-do-I-need?.js

---
+++ JS-8kyu-How-much-water-do-I-need?.js
@@ -0,0 +1,5 @@
+function howMuchWater(water, load, clothes){
+  if  (clothes > load * 2) return 'Too much clothes';
+  else if (clothes < load) return 'Not enough clothes';
+  else return Number((water * 1.1 ** (clothes - load)).toFixed(2))
+}
