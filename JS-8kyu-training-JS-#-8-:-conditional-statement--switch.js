8420d0e9d5b08de9b5b83e86a693201c9c98701e
Daniel Forero <99362093+ForeroDaniel@users.noreply.github.com>
2024-11-12 21:42:57 -0500

    Create JS-8kyu-training-JS-#-8-:-conditional-statement--switch.js

---
+++ JS-8kyu-training-JS-#-8-:-conditional-statement--switch.js
@@ -0,0 +1,10 @@
+function howManydays(month){
+  switch(month){
+      case 4: return 30;
+      case 6: return 30;
+      case 9: return 30;
+      case 11: return 30;
+      case 2: return 28;
+      default: return 31;
+  }
+}
