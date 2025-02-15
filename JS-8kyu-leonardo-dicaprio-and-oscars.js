89c00bf47f994e81eaa57a3c5d96a54f13eaf8ac
Daniel Forero <99362093+ForeroDaniel@users.noreply.github.com>
2025-02-14 21:45:08 -0500

    Create JS-8kyu-leonardo-dicaprio-and-oscars.js

---
+++ JS-8kyu-leonardo-dicaprio-and-oscars.js
@@ -0,0 +1,7 @@
+function leo(oscar){
+  switch(oscar){
+      case 88: return "Leo finally won the oscar! Leo is happy";
+      case 86: return "Not even for Wolf of wallstreet?!";
+      default: return oscar < 88 ? "When will you give Leo an Oscar?" : "Leo got one already!"
+      }
+}
