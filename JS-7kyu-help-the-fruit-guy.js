ff4b465fb19550bc49481ddfffcb430745eeda70
Daniel Forero <99362093+ForeroDaniel@users.noreply.github.com>
2025-03-31 15:25:43 -0500

    Create JS-7kyu-help-the-fruit-guy.js

---
+++ JS-7kyu-help-the-fruit-guy.js
@@ -0,0 +1,10 @@
+function removeRotten(bagOfFruits){
+  if (!bagOfFruits) return [];
+    return bagOfFruits.map(fruit => {
+        if (fruit.startsWith("rotten")) {
+            return fruit.slice(6).toLowerCase();
+        } else {
+            return fruit;
+        }
+    });
+}
