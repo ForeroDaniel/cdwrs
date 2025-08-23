29702da99fc6617643b21b8d0ac5aacd72bd0289
Daniel Forero <99362093+ForeroDaniel@users.noreply.github.com>
2025-08-23 12:52:42 -0500

    Create JS-7kyu-jumping-number.js

---
+++ JS-7kyu-jumping-number.js
@@ -0,0 +1,9 @@
+function jumpingNumber(n){
+  let arr = n.toString().split('')
+  for(i=0; i < arr.length-1; i++){
+    if(Math.abs(arr[i] - arr[i+1]) !== 1 ){
+      return 'Not!!'
+    }
+  }
+  return 'Jumping!!'
+}
